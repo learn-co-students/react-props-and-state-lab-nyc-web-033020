5dc08995d9672f150f54c7a3e760b1fbadba0f83
theluispestana <theluispestana@gmail.com>
2020-06-03 17:33:48 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: "all",
+      },
+    };
   }
+
+  onChangeType = (event) => {
+    const target = event.target;
+    // console.log("event: ", e.value);
+    this.setState({ filters: { type: target.value } });
+  };
+
+  onFindPetsClick = () => {
+    const url =
+      this.state.filters.type === "all"
+        ? "/api/pets"
+        : `/api/pets?type=${this.state.filters.type}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((pets) => this.setState({ pets: pets }));
+  };
+
+  onAdoptPet = (id) => {
+    const index = this.state.pets.findIndex((pet) => pet.id === id);
+    const pets = [...this.state.pets];
+    const pet = pets[index];
+    pet.isAdopted = true;
+    pets[index] = pet;
+    this.setState({ pets: pets });
+    console.log("updated pet: ", this.state.pets[index]);
+  };
 
   render() {
     return (
@@ -24,16 +51,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onFindPetsClick={this.onFindPetsClick}
+                onChangeType={this.onChangeType}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
