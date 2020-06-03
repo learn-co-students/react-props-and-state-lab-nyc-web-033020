0aa8859cd72ab2d3a6bf6f7b35cd678c1d458b0f
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-06-03 17:59:33 -0400

    3hr mark - a few deliverables left

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -2,19 +2,20 @@ import React from 'react'
 
 class Pet extends React.Component {
   render() {
+    console.log(this.props.name)
     return (
       <div className="card">
         <div className="content">
           <a className="header">
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.name}</p>
           </div>
         </div>
         <div className="extra content">
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,25 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  // const  {age, gender, id, isAdopted, name, type, weight} = props
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return (
+      <div className="ui cards">
+          {this.props.pets.map(pet => <Pet 
+              key={pet.id}
+              age={pet.age} 
+              gender={pet.gender}
+              id={pet.id}
+              isAdopted={pet.isAdopted}
+              name={pet.name}
+              type={pet.type}
+              weight={pet.weight}
+            />)
+          }
+      </div>
+    )
   }
 }
 
 export default PetBrowser
+
